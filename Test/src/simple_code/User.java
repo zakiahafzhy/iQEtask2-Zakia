c604e901670f521c5388a8293b5b984ccec453e4
zakiahafzhy <zakiahafzhy@it.student.pens.ac.id>
2021-11-06 12:14:24 +0700

    Print hello world

---
+++ Test/src/simple_code/User.java
@@ -0,0 +1,22 @@
+package simple_code;
+
+public class User {
+    String nama;
+    String noHP;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNoHP() {
+        return noHP;
+    }
+
+    public void setNoHP(String noHP) {
+        this.noHP = noHP;
+    }
+}
