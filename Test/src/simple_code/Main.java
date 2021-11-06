ac9fc8173b855e27715bfbbc4ee329e5de622591
zakiahafzhy <zakiahafzhy@it.student.pens.ac.id>
2021-11-06 13:14:24 +0700

    Add fitur user

--- Test/src/simple_code/Main.java
+++ Test/src/simple_code/Main.java
@@ -3,5 +3,11 @@ package simple_code;
 public class Main {
     public static void main (String[] args) {
         System.out.println("Hello World!");
+        User newUser = new User();
+
+        newUser.setNama("Zakia Hafzhy");
+        newUser.setNoHP("0123456789");
+
+        System.out.println(newUser.getInfo());
     }
 }
